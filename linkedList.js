cc83bae2da20a999f32f1278830a5adcfb607097
tuluiformdr <tului@formdr.com>
2022-05-30 12:33:03 -0700

    LL reverse practice

--- linkedList.js
+++ linkedList.js
@@ -26,24 +26,24 @@ node8.next = node9;
 
 const traverse = (startingNode) => {
   let currentNode = startingNode;
-  process.stdout.write(currentNode.val, '-> ');
+  process.stdout.write(currentNode.val, "-> ");
 
   while (currentNode.next) {
     currentNode = currentNode.next;
-    process.stdout.write(currentNode.val, '-> ');
+    process.stdout.write(currentNode.val, "-> ");
   }
-}
+};
 
 const recursiveTraverse = (currentNode) => {
-  process.stdout.write(`${currentNode.val} ${currentNode.next ? '-> ' : '\n'}`);
+  process.stdout.write(`${currentNode.val} ${currentNode.next ? "-> " : "\n"}`);
   if (currentNode.next) recursiveTraverse(currentNode.next);
-}
+};
 
 const insertAtBegin = (headNode, val) => {
   const newHead = new SinglyNode(val);
   newHead.next = headNode;
   return newHead;
-}
+};
 
 const insertAtEnd = (headNode, val) => {
   let lastNode = headNode;
@@ -54,13 +54,13 @@ const insertAtEnd = (headNode, val) => {
 
   lastNode.next = new SinglyNode(val);
   return lastNode.next;
-}
+};
 
 const insertAt = (prevNode, val) => {
   const newNode = new SinglyNode(val);
   newNode.next = prevNode.next;
   prevNode.next = newNode;
-}
+};
 
 const deleteNode = (currentNode, delNode) => {
   if (currentNode === delNode) currentNode.next = null;
@@ -72,7 +72,7 @@ const deleteNode = (currentNode, delNode) => {
     }
     currentNode = currentNode.next;
   }
-}
+};
 
 const searchNode = (currentNode, searchVal) => {
   while (currentNode.next) {
@@ -83,107 +83,62 @@ const searchNode = (currentNode, searchVal) => {
     currentNode = currentNode.next;
   }
   return false;
-}
+};
 
 const updateNode = (currentNode, oldVal, newVal) => {
   while (currentNode.next) {
     if (currentNode.val === oldVal) currentNode.val = newVal;
     currentNode = currentNode.next;
   }
-}
+};
 
 const node0 = insertAtBegin(node1, 0); // adds node0 at the beginning
 const node10 = insertAtEnd(node0, 10); // adds node10 at the end
-insertAt(node6, 6.5);  // adds node6.5 after node6
-insertAt(node6, 6.5);  // adds node6.5 after node6
-insertAt(node6, 6.5);  // adds node6.5 after node6
-deleteNode(node0, node10);  // deletes node10 at the end
+insertAt(node6, 6.5); // adds node6.5 after node6
+insertAt(node6, 6.5); // adds node6.5 after node6
+insertAt(node6, 6.5); // adds node6.5 after node6
+deleteNode(node0, node10); // deletes node10 at the end
 updateNode(node0, 6.5, 6.9);
 
-console.log('\n');
-console.log('<------------------- START ------------------->');
-console.log('\n   <------------- regular ------------->');
+console.log("\n");
+console.log("<------------------- START ------------------->");
+console.log("\n   <------------- regular ------------->");
 recursiveTraverse(node0);
-console.log('   <------------- regular ------------->');
+console.log("   <------------- regular ------------->");
 
-const reverseLL = (head) => {
-  let next = null;
-  let prev = null;
+const reverseLL = (currentNode) => { // non recursive solution
+  let tempNext = null;
+  let tempPrev = null;
 
-  while (head) {
-    next = head.next;
-    head.next = prev;
-    prev = head;
-    head = next;
-  }
-  return prev;
-}
+  while(currentNode) {
+    tempNext = currentNode.next; // saving next
+    currentNode.next = tempPrev; // pointing next to previous node
 
-
+    tempPrev = currentNode;
+    currentNode = tempNext; // last two lines move to the next node
+  };
+  return tempPrev;
+};
 
 
 reverseLL(node0);
+console.log("\n   <------------- reverse (1x) ------------->");
+recursiveTraverse(node9);
+console.log("   <------------- reverse (1x) ------------->");
 
+const reverseLLR = (currentNode, prev = null) => {
+  if (!currentNode) return prev;
 
-const recursiveRLL = (head, prev) => {
-  if (!head) return prev;
-
-  const next = head.next;
-  head.next = prev;
-
-  return recursiveRLL(next, head);
+  const tempNext = currentNode.next;
+  currentNode.next = prev;
+  
+  return reverseLLR(tempNext, currentNode);
 }
 
-
-
-
-recursiveRLL(node9);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const reverseList = (currentNode, nodes = []) => {
-//   let next = null;
-//   let prev = null;
-
-//   while (currentNode) {
-//     next = currentNode.next;
-//     currentNode.next = prev;
-
-//     prev = currentNode;
-//     currentNode = next;
-//   }
-//   return prev;
-// }
-
-// const recursiveReverse = (head, prev = null) => {
-//   if (!head.next) {
-//     head.next = prev;
-//     return head;
-//   }
-//   const last = recursiveReverse(head.next, head);
-//   head.next = prev;
-//   return last;
-// }
-
-// recursiveReverse(node0);
-
-console.log('\n   <------------- reverse (1x) ------------->');
-recursiveTraverse(node9);
+console.log("\n   <------------- reverse (2x) ------------->");
+reverseLLR(node9);
 recursiveTraverse(node0);
-console.log('   <------------- reverse (1x) ------------->');
-console.log('\n');
-console.log('<------------------- END ------------------->\n');
+console.log("   <------------- reverse (2x) ------------->");
 
 
+console.log("\n<------------------- END ------------------->\n");

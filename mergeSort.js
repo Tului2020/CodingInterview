1c01d3423c3b5c9195ded3545f177403a79406ba
tuluiformdr <tului@formdr.com>
2022-06-04 15:36:10 -0700

    merge sort done

---
+++ mergeSort.js
@@ -0,0 +1,28 @@
+const mergeSort = (unsortedArr) => {
+  if (unsortedArr.length < 2) return unsortedArr;
+
+  const midIdx = Math.floor(unsortedArr.length / 2);
+  const left = mergeSort(unsortedArr.slice(0, midIdx));
+  const right = mergeSort(unsortedArr.slice(midIdx));
+
+  return mergeHelper(left, right);
+}
+
+
+const mergeHelper = (leftArr, rightArr) => {
+  const mergedArray = [];
+
+  while (leftArr.length && rightArr.length) {
+    mergedArray.push((leftArr[0] > rightArr[0] ? rightArr : leftArr).shift());
+  }
+
+  return mergedArray.concat(leftArr).concat(rightArr);
+}
+
+console.log(mergeSort([34, 321, 4, 324, 1,31,4,32,432,14,3,214,31,2]));
+
+
+
+
+
+

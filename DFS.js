b29579c14f9cc56d0de6f2610b2364b48b403917
tuluiformdr <tului@formdr.com>
2022-06-04 11:19:08 -0700

    BFS and DFS done

--- DFS.js
+++ DFS.js
@@ -1,23 +1,44 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.left = null;
-    this.right = null;
+const { a } = require('./BinaryTree');
+
+const depthFirstTraverseR = (node) => {
+  if (!node) return;
+
+  console.log(node.val);
+  depthFirstTraverseR(node.left);
+  depthFirstTraverseR(node.right);
+}
+
+// depthFirstTraverseR(a);
+// console.log('<-------------->');
+
+const depthFirstTraverseNR = (headNode) => {
+  const stack = [headNode];
+
+  while (stack.length) {
+    const lastEl = stack.pop();
+    console.log(lastEl.val);
+    if (lastEl.right) stack.push(lastEl.right);
+    if (lastEl.left) stack.push(lastEl.left);
   }
 }
 
-const a = new Node(3);
-const b = new Node(2);
-const c = new Node(7);
-const d = new Node(4);
-const e = new Node(-2);
-const f = new Node(5);
+// depthFirstTraverseNR(a);
 
-a.left = b;
-a.right = c;
-b.left = d;
-b.right = e;
-c.right = f;
+// const depthFirstSearch = (node, target) => {
+//   if (!node) return;
+//   if (node.val === target) return true;
+//   return !!(depthFirstSearch(node.left, target) || depthFirstSearch(node.right, target));
+// }
 
+// console.log(depthFirstSearch(a, -100));
+
+
+const sumTree = (node) => {
+  if (!node) return 0;
+
+  return node.val + sumTree(node.left) + sumTree(node.right);
+}
+
+console.log(sumTree(a));
 
 

94a632c126fd0511fa68115fcadfc7fa08839c81
tuluiformdr <tului@formdr.com>
2022-06-05 16:31:09 -0700

    redid 15_3sum

--- 15_3sum.js
+++ 15_3sum.js
@@ -1,23 +1,22 @@
-const threeSum = (nums) => {
+const threeSum = (nums, target = 0) => {
   const results = [];
   nums = nums.sort((a, b) => a - b);
-  for (let i = 0; i < nums.length; i++) {
-    if (0 < i && nums[i] === nums[i - 1]) continue;
-    const target = -nums[i];
-
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    const _target = target - nums[i];
     let left = i + 1;
     let right = nums.length - 1;
 
     while (left < right) {
       const sum = nums[left] + nums[right];
-      if (target < sum) {
+      if (_target < sum) {
         right--;
-      } else if (target > sum) {
+      } else if (_target > sum) {
         left++;
       } else {
         results.push([nums[i], nums[left], nums[right]]);
-        while(nums[right] === nums[right - 1]) right--;
-        while(nums[left] === nums[left + 1]) left++;
+        while (nums[right] === nums[right - 1]) right--;
+        while (nums[left] === nums[left + 1]) left++;
         left++;
         right--;
       }
